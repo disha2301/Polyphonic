5578c0f5c1259c663cca955f48a94a373c95d94b
Disha <dishay166@gmail.com>
2024-04-27 12:46:14 +0530

    :zap: add download functionality

--- src/components/AudioPlayer.jsx
+++ src/components/AudioPlayer.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { AiFillPlayCircle } from "react-icons/ai";
 import { BsPauseCircleFill } from "react-icons/bs";
 import { BiSolidDownload } from "react-icons/bi";
@@ -21,6 +21,33 @@ const AudioPlayer = ({ audioFile }) => {
     setIsPlaying(!isPlaying);
   };
 
+  const updateProgressBar = () => {
+    const audio = audioRef.current;
+    const progress = (audio.currentTime / audio.duration) * 100;
+    setCurrentTime(audio.currentTime);
+    progressBarRef.current.style.width = `${progress}%`;
+  };
+
+  const downloadAudio = () => {
+    if (audioFile && audioFile.AudioStream && audioFile.AudioStream.buffer) {
+      const audioArrayBuffer = audioFile.AudioStream.buffer;
+      const audioURL = URL.createObjectURL(
+        new Blob([audioArrayBuffer], { type: "audio/mpeg" })
+      );
+
+      const a = document.createElement("a");
+      a.href = audioURL;
+      a.download = "audio.mp3";
+      a.style.display = "none";
+
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+
+      URL.revokeObjectURL(audioURL);
+    }
+  };
+
   useEffect(() => {
     if (audioFile && audioFile.AudioStream && audioFile.AudioStream.buffer) {
       const audioArrayBuffer = audioFile.AudioStream.buffer;
@@ -30,42 +57,24 @@ const AudioPlayer = ({ audioFile }) => {
 
       const audio = audioRef.current;
       audio.src = audioURL;
-      audio.addEventListener("loaddata", () => {
+      audio.addEventListener("loadeddata", () => {
         setDuration(audio.duration);
       });
       audio.addEventListener("timeupdate", updateProgressBar);
 
       return () => {
+        audio.removeEventListener("loadeddata", () => {
+          setDuration(audio.duration);
+        });
+        audio.removeEventListener("timeupdate", updateProgressBar);
         URL.revokeObjectURL(audioURL);
       };
     }
   }, [audioFile]);
-  const updateProgressBar = () => {
-    const audio = audioRef.current;
-    const progress = (audio.currentTime / audio.duration) * 100;
-    setCurrentTime(audio.currentTime);
-    progressBarRef.current.style.width = `${progress}%`;
-  };
-  const downloadAudio = () => {
-    if (audioFile) {
-      const audioArrayBuffer = audioFile.AudioStream.buffer;
-      const audioURL = URL.createObjectURL(
-        new Blob([audioArrayBuffer], { type: "audio/mpeg" })
-      );
-      const a = document.createElement("a");
-      a.href = audioURL;
-      a.download = "audio.mp3";
-      a.style.display = "none";
-      document.appendChild(a);
-      a.click();
-      document.body.removeChild(a);
-      URL.revokeObjectURL(audioURL);
-    }
-  };
 
   return (
     <div className="audio-container">
-      <audio ref={audioRef} controls />{" "}
+      <audio ref={audioRef} controls />
       <div className="progress-container">
         <div
           ref={progressBarRef}
@@ -73,11 +82,10 @@ const AudioPlayer = ({ audioFile }) => {
           style={{ width: `${(currentTime / duration) * 100}%` }}
         />
       </div>
-      {/* Use controls for default audio player controls */}
       <button
         className="audio-button"
         disabled={!audioFile}
-        onClick={() => togglePlay()}
+        onClick={togglePlay}
       >
         {isPlaying ? (
           <BsPauseCircleFill className="icon-btn" />
@@ -88,7 +96,7 @@ const AudioPlayer = ({ audioFile }) => {
       <button
         className="audio-button"
         disabled={!audioFile}
-        onClick={() => downloadAudio()}
+        onClick={downloadAudio}
       >
         <BiSolidDownload className="icon-btn" />
       </button>
